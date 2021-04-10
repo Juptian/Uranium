d9afdc263798c13157c407fb59a3526fe81cf16b
Juptian <juptianraffles@gmail.com>
2021-04-10 18:29:42 -0400

    Update UnitTest1.cs

--- Source/Compiler.Tests/UnitTest1.cs
+++ Source/Compiler.Tests/UnitTest1.cs
@@ -22,7 +22,7 @@ namespace Compiler.Tests
         {
             Assert.AreEqual(TokenType.CloseCurlyBrace, lexer.Lex(text1[0]) );
             Assert.AreEqual(TokenType.LineBreak, lexer.Lex(text1[1]));
-            Assert.AreEqual(TokenType.LineBreak, lexer.Lex(text1[2]));
+
             Assert.AreEqual(TokenType.Plus, lexer.Lex(text1[3]));
             Assert.AreEqual(TokenType.Equals, lexer.Lex(text1[4]));
             Assert.AreEqual(TokenType.Semicolon, lexer.Lex(text1[5]));
