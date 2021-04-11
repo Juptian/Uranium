9f945f738b680eaac79b35666d47bb9f6536d276
Juptian <juptianraffles@gmail.com>
2021-04-11 11:16:57 -0400

    Update ErrorLogger.cs
    Fixed bugs

--- Source/Compiler/Logging/ErrorLogger.cs
+++ Source/Compiler/Logging/ErrorLogger.cs
@@ -5,14 +5,14 @@ namespace Compiler.Logging
 {
     public static class ErrorLogger
     {
-        public static void ReportInvalidNumber([CallerLineNumber] int source = 0)
+        public static void ReportInvalidNumber([CallerLineNumber] int position = 0)
         {
-            throw new Exception("JC0001: Error at index: " + source + @" 
+            throw new Exception("JC0001: Error at index: " + position + @" 
 Cannot have an invalid number, this includes having multiple .'s in a number, or a number that is too large!");
         }
         public static void ReportNumberStartingWith_([CallerLineNumber] int position = 0)
         {
-            throw new Exception("JC002: Error at index: " + source + @"
+            throw new Exception("JC002: Error at index: " + position + @"
 Cannot have a number starting with an underscore (_) !");
         }
     }
