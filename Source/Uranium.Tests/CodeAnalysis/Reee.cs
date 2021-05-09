5d4e536edd44f5b78a9e4a36575b846358e801f2
Juptian <juptianraffles@gmail.com>
2021-05-09 18:17:16 -0400

    Testing codecov switch expressions

---
+++ Source/Uranium.Tests/CodeAnalysis/Reee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uranium.CodeAnalysis;
+using Xunit;
+
+namespace Uranium.Tests.CodeAnalysis
+{
+    public static class Reee
+    {
+        [Fact]
+        public static void ABc()
+        {
+            var a = Test.TestOne(10);
+            Assert.Equal(10, a);
+        }
+    }
+}
---
+++ Source/Uranium/CodeAnalysis/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uranium.CodeAnalysis
+{
+    public static class Test
+    {
+        public static int TestOne(int a)
+            => a switch
+            {
+                _ => testTwo(),
+            };
+        private static int testTwo()
+            => 10;
+    }
+}
