e4f4daab57dc112b7710abe7a3da7d3d16673171
Juptian <juptianraffles@gmail.com>
2021-11-05 20:05:58 -0400

    Added a run.sh script for linux users
    I've recently swapped over to linux, so now I have a script to simplify the lives of linux users!

---
+++ run.sh
@@ -0,0 +1,3 @@
+dotnet build Uranium.sln --configuration Release
+dotnet test Uranium.sln
+dotnet run  --project ./Source/Uranium.Main/Uranium.Main.csproj Test.urnm --ShowTree --boundTree --c Release
